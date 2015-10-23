2d9cc361a0d074f34571f5c4155a747aec825229
Adrian Ber <beradrian@yahoo.com>
2015-10-23 16:51:24 +0300

    refactoring

--- spec/jscommonSpec.js
+++ spec/jscommonSpec.js
@@ -1,15 +1,15 @@
 var jscommon = require("../src/main/js/jscommon.js");
 
 describe("jscommon", function() {
-	describe("removeElementFromArray", function() {
+	describe("remove", function() {
 		it("should remove existing", function() {
 			var arr = [1, 2, 3];
-			jscommon.removeElementFromArray(arr, 2);
+			jscommon.remove(arr, 2);
 			expect(arr).toEqual([1, 3]);
 		});
 		it("shouldn't remove non-existing", function() {
 			var arr = [1, 2, 3];
-			jscommon.removeElementFromArray(arr, 4);
+			jscommon.remove(arr, 4);
 			expect(arr).toEqual([1, 2, 3]);
 		});
 	});
